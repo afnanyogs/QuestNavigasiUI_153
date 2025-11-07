ec2d459ce7a2a707241ce81bfd594f600f5bdbb0
Afnan Asilah <afnanyogs@gmail.com>
2025-11-07 10:18:29 +0700

    nama panggilan yang akan Anda gunakan di file build.gradle modul

--- app/src/main/java/com/example/navigasi/coba/Formulir.kt
+++ app/src/main/java/com/example/navigasi/coba/Formulir.kt
@@ -1,2 +1,12 @@
 package com.example.navigasi.coba
 
+import android.R
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn( markerClass = [ExperimentalMaterial3Api::class])
+@Composable
+fun formIsian(
+    jenisK: List<String> =listOf("Laki-laki","Perempuan")
+    OnSubmitBtnClick : () -> Unit
+){ isiruang}
